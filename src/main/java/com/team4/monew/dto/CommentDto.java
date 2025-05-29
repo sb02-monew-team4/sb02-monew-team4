29045df10a360944d2a16d4d380aa948b0b15d91
ddday366 <ddday366@gmail.com>
2025-05-29 16:11:39 +0900

    feat: CommentDto 구현

---
+++ src/main/java/com/team4/monew/dto/CommentDto.java
@@ -0,0 +1,16 @@
+package com.team4.monew.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record CommentDto(
+    UUID id,
+    UUID articleId,
+    UUID userId,
+    String userNickname,
+    String content,
+    int likeCount,
+    boolean likeByMe,
+    Instant createdAt
+) {
+}
