4e08bb138095fe02642003e4790a151ebff1d7f9
ddday366 <ddday366@gmail.com>
2025-06-04 11:36:51 +0900

    feat: CommentMapper 구현

---
+++ src/main/java/com/team4/monew/mapper/CommentMapper.java
@@ -0,0 +1,18 @@
+package com.team4.monew.mapper;
+
+import com.team4.monew.dto.comment.CommentDto;
+import com.team4.monew.entity.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+  @Mapping(target = "articleId", source = "news.id")
+  @Mapping(target = "userId", source = "user.id")
+  @Mapping(target = "userNickname", source = "user.nickname")
+  @Mapping(target = "likeCount", expression = "java(comment.getLikeCount() != null ? comment.getLikeCount().intValue() : 0)")
+  @Mapping(target = "likeByMe", constant = "false")
+  CommentDto toDto(Comment comment);
+}
+
