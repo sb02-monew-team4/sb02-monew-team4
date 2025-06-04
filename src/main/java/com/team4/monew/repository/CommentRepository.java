3c546dea7f23d80eb88541296c5ec92265d92cb1
ddday366 <ddday366@gmail.com>
2025-06-04 11:36:25 +0900

    feat: CommentRepository 구현

---
+++ src/main/java/com/team4/monew/repository/CommentRepository.java
@@ -0,0 +1,22 @@
+package com.team4.monew.repository;
+
+import com.team4.monew.entity.Comment;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+  Page<Comment> findByUserIdAndIsDeletedFalse(UUID userId, Pageable pageable);
+  List<Comment> findCommentsByArticleWithCursorPaging(
+      UUID newsId,
+      String orderBy,
+      String direction,
+      String cursor,
+      String after,
+      int limit
+  );
+
+  long countByNewsId(UUID newsId);
+}
