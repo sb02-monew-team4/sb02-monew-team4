3c320c29d93f03d700ab477c53247413867b07ae
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-06-05 15:37:37 +0900

    feature: Exception 구현

--- src/main/java/com/team4/monew/exception/ErrorCode.java
+++ src/main/java/com/team4/monew/exception/ErrorCode.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
   // USER
-  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found");
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+
+
+  // USER_ACTIVITY
+  USER_ACTIVITY_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 활동 내역이 없습니다"),
+  USER_ACTIVITY_NOT_FOUND_IN_MONGO(HttpStatus.NOT_FOUND, "MongoDB에서 유저 활동 내역을 찾을 수 없습니다.");
 
 
   private HttpStatus status;
