fc742fc4da90470eb15ed1cd62e6995929780f1e
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-06-05 15:32:14 +0900

    feature: MongoDB용 활동내역 및 구독 엔티티 개발

---
+++ src/main/java/com/team4/monew/entity/Subscription.java
@@ -0,0 +1,43 @@
+package com.team4.monew.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+
+@Entity
+@Getter
+@Table(name = "subscriptions")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Subscription {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "interest_id", nullable = false)
+  private Interest interest;
+
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private Instant createdAt;
+}
+
