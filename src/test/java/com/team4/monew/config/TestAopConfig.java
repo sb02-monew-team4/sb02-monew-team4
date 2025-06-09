05fbe8184d0f69310f180be6de1b601ffde6f5a9
hanna kong <khn1346@gmail.com>
2025-06-09 10:29:03 +0900

    feat: 인증 인터셉터, 인가 AOP 포함한 테스트

---
+++ src/test/java/com/team4/monew/config/TestAopConfig.java
@@ -0,0 +1,10 @@
+package com.team4.monew.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@TestConfiguration
+@EnableAspectJAutoProxy
+public class TestAopConfig {
+
+}
