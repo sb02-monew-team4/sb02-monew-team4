89e2ea231430a77092c62672a1b2b109931f5237
ddday366 <ddday366@gmail.com>
2025-06-10 13:35:21 +0900

    fix: InterestRepository 수정

--- src/main/java/com/team4/monew/repository/InterestRepository.java
+++ src/main/java/com/team4/monew/repository/InterestRepository.java
@@ -1,9 +1,20 @@
 package com.team4.monew.repository;
 
 import com.team4.monew.entity.Interest;
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterestRepository extends JpaRepository<Interest, UUID> {
+  List<Interest> findByKeywordWithCursorPaging(
+      String keyword,
+      String orderBy,
+      String direction,
+      UUID cursor,
+      Instant after,
+      int limit
+  );
 
+  long countByKeyword(String keyword);
 }
