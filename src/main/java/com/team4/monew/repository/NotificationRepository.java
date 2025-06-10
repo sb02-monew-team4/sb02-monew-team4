516f3dceb369284deab42f32b5c0d7528bbf6ee5
hanna kong <khn1346@gmail.com>
2025-06-10 11:22:17 +0900

    feat: 알림 삭제 스케줄러 구현

--- src/main/java/com/team4/monew/repository/NotificationRepository.java
+++ src/main/java/com/team4/monew/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.team4.monew.repository;
 
 import com.team4.monew.entity.Notification;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import org.springframework.stereotype.Repository;
 public interface NotificationRepository extends JpaRepository<Notification, UUID>, NotificationRepositoryCustom {
   List<Notification> findByUserIdAndConfirmedFalse(UUID userId);
   long countByUserIdAndConfirmedFalse(UUID userId);
+  long deleteByConfirmedTrueAndCreatedAtBefore(Instant cutoffDateTime);
 }
