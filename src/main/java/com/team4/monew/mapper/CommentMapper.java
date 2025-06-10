e3bf2d61f17f60aaeac3316a993e98949a5d0cd9
ddday366 <ddday366@gmail.com>
2025-06-10 13:38:05 +0900

    fix: CommentMapper 수정

--- src/main/java/com/team4/monew/mapper/CommentMapper.java
+++ src/main/java/com/team4/monew/mapper/CommentMapper.java
@@ -2,6 +2,8 @@ package com.team4.monew.mapper;
 
 import com.team4.monew.dto.comment.CommentDto;
 import com.team4.monew.entity.Comment;
+import java.util.UUID;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,6 +15,6 @@ public interface CommentMapper {
   @Mapping(target = "userNickname", source = "user.nickname")
   @Mapping(target = "likeCount", expression = "java(comment.getLikeCount() != null ? comment.getLikeCount().intValue() : 0)")
   @Mapping(target = "likeByMe", constant = "false")
-  CommentDto toDto(Comment comment);
+  CommentDto toDto(Comment comment, @Context UUID requesterId);
 }
 
