d56e7a18d8f33d41e97f1c3fa49313ffe8913ca6
JJHyunDev <bellhyunforgithub@gmail.com>
2025-06-12 09:44:42 +0900

    feat: ArticleController 삭제, 출처 목록 조회, 기사 목록 조회 구현

--- src/main/java/com/team4/monew/controller/ArticleController.java
+++ src/main/java/com/team4/monew/controller/ArticleController.java
@@ -1,10 +1,18 @@
 package com.team4.monew.controller;
 
+import com.team4.monew.dto.article.ArticleSearchRequest;
 import com.team4.monew.dto.article.ArticleViewDto;
+import com.team4.monew.dto.article.CursorPageResponseArticleDto;
 import com.team4.monew.service.ArticleService;
+import jakarta.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -18,14 +26,60 @@ public class ArticleController {
 
   private final ArticleService articleService;
 
-  @PostMapping("/api/articles/{newsId}/article-views")
+  @PostMapping("/api/articles/{articleId}/article-views")
   public ResponseEntity<ArticleViewDto> register(
-      @PathVariable UUID newsId,
+      @PathVariable UUID articleId,
       @RequestHeader("Monew-Request-User-ID") UUID userId
   ) {
-    ArticleViewDto response = articleService.registerNewsView(newsId, userId);
+    ArticleViewDto response = articleService.registerArticleView(articleId, userId);
 
     return ResponseEntity.ok(response);
   }
+
+  @Transactional
+  @GetMapping("/api/articles")
+  public ResponseEntity<CursorPageResponseArticleDto> getArticles(
+      @Valid @ModelAttribute ArticleSearchRequest request,
+      @RequestHeader("Monew-Request-User-ID") UUID userId
+  ) {
+    CursorPageResponseArticleDto response = articleService.getAllArticles(request, userId);
+
+    return ResponseEntity.ok(response);
+  }
+
+  @Transactional
+  @GetMapping("/api/articles/sources")
+  public ResponseEntity<List<String>> getSources() {
+    List<String> sources = articleService.getAllSources();
+
+    return ResponseEntity.ok(sources);
+  }
+
+//  @Transactional
+//  @GetMapping("/api/articles/restore")
+//  public ResponseEntity<ArticleRestoreResultDto> restore(
+//      @RequestParam Instant from,
+//      @RequestParam Instant to
+//  ) {
+//    ArticleRestoreResultDto response = articleService.restore();
+//
+//    return ResponseEntity.ok(response);
+//  }
+
+  @Transactional
+  @DeleteMapping("/api/articles/{articleId}")
+  public ResponseEntity<Void> softDelete(@PathVariable UUID articleId) {
+    articleService.softDelete(articleId);
+
+    return ResponseEntity.noContent().build();
+  }
+
+  @Transactional
+  @DeleteMapping("/api/articles/{articleId}/hard")
+  public ResponseEntity<Void> hardDelete(@PathVariable UUID articleId) {
+    articleService.hardDelete(articleId);
+    return ResponseEntity.noContent().build();
+  }
+
 
 }
