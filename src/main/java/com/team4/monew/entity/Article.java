18ed2c3be65f025c63346f8f41afc98efbbf1701
hanna kong <khn1346@gmail.com>
2025-06-16 11:18:33 +0900

    fix: table name articles로 수정

--- src/main/java/com/team4/monew/entity/Article.java
+++ src/main/java/com/team4/monew/entity/Article.java
@@ -23,7 +23,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name = "article")
+@Table(name = "articles")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
