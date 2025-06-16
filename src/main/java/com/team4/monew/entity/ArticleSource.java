009db35af5b04e621f28a367b38fe47f7bb5240d
hanna kong <khn1346@gmail.com>
2025-06-16 17:12:37 +0900

    fix: 연합뉴스 RSS 주소 수정

--- src/main/java/com/team4/monew/entity/ArticleSource.java
+++ src/main/java/com/team4/monew/entity/ArticleSource.java
@@ -3,7 +3,7 @@ package com.team4.monew.entity;
 public enum ArticleSource {
   HANKYUNG("한국경제", "https://www.hankyung.com/feed/all-news"),
   CHOSUN("조선일보", "https://www.chosun.com/arc/outboundfeeds/rss/?outputType=xml"),
-  YONHAP("연합뉴스", "http://www.yonhapnewstv.co.kr/browse/feed/");
+  YONHAP("연합뉴스", "https://www.yna.co.kr/rss/news.xml");
 
   private final String source;
   private final String rssUrl;
